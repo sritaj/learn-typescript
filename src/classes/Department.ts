ec9fb9c087142eae56e6d316c096f4abd5ef907f
Sritaj <sritajp@gmail.com>
2022-05-27 23:31:16 +0530

    Added example for private keyword in typescript

--- src/classes/Department.ts
+++ src/classes/Department.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  private name: string;
+  private employee: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -11,11 +12,24 @@ class Department {
   describeDepartment() {
     console.log(`This ${this.name} has been established in the year 1990`);
   }
+
+  addEmployee(emp: string) {
+    this.employee.push(emp);
+  }
+
+  printEmployee() {
+    console.log(this.employee.length);
+    console.log(this.employee);
+  }
 }
 
 const departName = new Department("Quality Engineer");
 departName.describeDepartment();
 console.log(departName);
+departName.addEmployee("Sritaj");
+departName.addEmployee("Max");
+
+departName.printEmployee();
 
 //specifying name key explicitly for departChoosen object, since describeDepartment() refers to name using this keyword; and for the below context if departChoosen doesn't have any name key then once the depart function is called it won't find name specified in the global context resulting in undefined value
 const departChoosen = {
