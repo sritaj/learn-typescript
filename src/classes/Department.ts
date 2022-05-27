212db3a11401cd5d6af6037ca25575490d5aead4
Sritaj <sritajp@gmail.com>
2022-05-27 23:37:29 +0530

    Added example for showcasing constructor shorthand initialisation in typescript

--- src/classes/Department.ts
+++ src/classes/Department.ts
@@ -2,15 +2,21 @@ class Department {
   private name: string;
   private employee: string[] = [];
 
-  constructor(n: string) {
+  //added example to showcase shortand of creation constructor with relevant variables along with regulr initialisation
+  constructor(private id: string, n: string) {
     this.name = n;
   }
+
+  //added example to showcase shortand of creation constructor with relevant variables
+  //constructor(private id: string, private name: string, private year: string){}
 
   /**
    * Function to print Department name using this reference
    */
   describeDepartment() {
-    console.log(`This ${this.name} has been established in the year 1990`);
+    console.log(
+      `This ${this.name} has been established in the year 1990 and registered with ID ${this.id}`
+    );
   }
 
   addEmployee(emp: string) {
@@ -23,7 +29,7 @@ class Department {
   }
 }
 
-const departName = new Department("Quality Engineer");
+const departName = new Department("1XA", "Quality Engineer");
 departName.describeDepartment();
 console.log(departName);
 departName.addEmployee("Sritaj");
@@ -33,6 +39,7 @@ departName.printEmployee();
 
 //specifying name key explicitly for departChoosen object, since describeDepartment() refers to name using this keyword; and for the below context if departChoosen doesn't have any name key then once the depart function is called it won't find name specified in the global context resulting in undefined value
 const departChoosen = {
+  id: "XM10",
   name: "Backend Engineers",
   depart: departName.describeDepartment,
 };
