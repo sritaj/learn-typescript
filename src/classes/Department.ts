f64f46faebc745a65f3e8da3ebe33e656d878a7a
Sritaj <sritajp@gmail.com>
2022-05-27 23:18:38 +0530

    Updated class with functional and this keyword example in typescript

--- src/classes/Department.ts
+++ src/classes/Department.ts
@@ -4,7 +4,22 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  /**
+   * Function to print Department name using this reference
+   */
+  describeDepartment() {
+    console.log(`This ${this.name} has been established in the year 1990`);
+  }
 }
 
 const departName = new Department("Quality Engineer");
+departName.describeDepartment();
 console.log(departName);
+
+//specifying name key explicitly for departChoosen object, since describeDepartment() refers to name using this keyword; and for the below context if departChoosen doesn't have any name key then once the depart function is called it won't find name specified in the global context resulting in undefined value
+const departChoosen = {
+  name: "Backend Engineers",
+  depart: departName.describeDepartment,
+};
+console.log(departChoosen.depart());
